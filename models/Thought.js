0102be7bcc9943598d3fac81253852d034061213
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-10-13 22:36:52 -0400

    added code to user.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,37 +2,7 @@ const { Schema, model, Types } = require("mongoose");
 const moment = require("moment");
 const reactionSchema = require('./Reaction');
 
-//create reaction schema as a subdocument schema for thought schema model
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) =>
-        moment(createdAtVal).format("MMM D, YYYY [at] HH:mm a"),
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-    id: false,
-    timestamps: true,
-  }
-);
+
 
 //thought model
 const thoughtSchema = new Schema(
